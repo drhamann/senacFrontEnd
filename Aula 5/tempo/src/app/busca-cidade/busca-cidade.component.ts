8315c67dca899bcac6d6e374a4a8193315b5408f
George <george.hamann@gmail.com>
2022-05-25 16:08:39 -0300

    feat: new information to the class

--- Aula 5/tempo/src/app/busca-cidade/busca-cidade.component.ts
+++ Aula 5/tempo/src/app/busca-cidade/busca-cidade.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+import { TempoService } from '../tempo/tempo.service';
 
 @Component({
   selector: 'app-busca-cidade',
@@ -8,11 +10,18 @@ import { FormControl } from '@angular/forms';
 })
 export class BuscaCidadeComponent implements OnInit {
 
-  busca = new FormControl()
+  busca = new FormControl('', [Validators.minLength(2)])
 
-  constructor() { }
+  constructor(private tempoService: TempoService) { }
 
   ngOnInit(): void {
+    this.busca.valueChanges.subscribe((valorDaBusca: string) => {
+      if (valorDaBusca) {
+        const valorDoInput = valorDaBusca.split(',').map(letra => letra.trim())
+        this.tempoService.getCurrentWeather(valorDoInput[0], valorDoInput.length > 1 ? valorDoInput[1] : undefined).pipe(debounceTime(1000))
+          .subscribe(data => (console.log(data)))
+      }
+    })
   }
 
 }
--- Aula 5/tempo/src/app/tempo/tempo.service.ts
+++ Aula 5/tempo/src/app/tempo/tempo.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ICurrentWeatherData, ITempoAtual } from '../interfaces';
+import { Coordenadas, ICurrentWeatherData, ITempoAtual } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +11,36 @@ export class TempoService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getCurrentWeather(city: string, country: string): Observable<ITempoAtual> {
+  getCurrentWeather(busca: string | number, pais?: string): Observable<ITempoAtual> {
+    let uriParams = ''
+    if (typeof busca === 'string') {
+      uriParams = `q=${busca}`
+    } else {
+      uriParams = `zip=${busca}`
+    }
+    if (pais) {
+      uriParams = `${uriParams},${pais}`
+    }
+    return this.getCurrentWeatherHelper(uriParams)
+  }
+
+  private getCurrentWeatherHelper(uriParams: string): Observable<ITempoAtual> {
+    return this.httpClient.get<ICurrentWeatherData>(
+      `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?${uriParams}&appid=${environment.appId}`)
+      .pipe(map(data => this.transformToITempoAtual(data)))
+  }
+
+  getCurrentWeatherByCoords(coords: Coordenadas): Observable<ITempoAtual> {
+    const uriParams = `lat=${coords.latitude}&lon=${coords.longitude}`
+    return this.getCurrentWeatherHelper(uriParams)
+  }
+
+  /*getCurrentWeather(city: string, country: string): Observable<ITempoAtual> {
     return this.httpClient.get<ICurrentWeatherData>(
       `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?` +
       `q=${city},${country}&appid=${environment.appId}`
     ).pipe(map(data => this.transformToITempoAtual(data)))
-  }
+  }*/
 
   private transformToITempoAtual(data: ICurrentWeatherData): ITempoAtual {
     console.log(data)
