a2d49cb56f7e93b16050df1e444c8cb766049f66
George <george.hamann@gmail.com>
2022-06-29 17:40:08 -0300

    fix: adjust html

--- Aulas/Projeto/Authentication/Authentication/ClientApp/src/app/modulos/users/users-home/users-home.component.ts
+++ Aulas/Projeto/Authentication/Authentication/ClientApp/src/app/modulos/users/users-home/users-home.component.ts
@@ -14,14 +14,8 @@ export class UsersHomeComponent implements OnInit {
   total: number = 25;
   filterPlanograms: string[] = [];
 
-  constructor(
-    private router: Router,
-    public usersService: UsersService
-  ) {
-    //this.usersService.getUsers().subscribe(data => this.usersService.usersData.next(data));
-  }
+  constructor(private router: Router, public usersService: UsersService) { }
   ngOnInit(): void {
-    //this.users = JSON.parse('[{"id":"18c77172-a411-4d89-9393-bbe9d4c80b81","email":"batman@test.com","userName":"batman","password":"batman123456","role":"simples"},{"id":"3647bcf1-c696-4ee3-8edd-aaa032b05eb6","email":"robin@test.com","userName":"robin","password":"robin123456","role":"simples"},{"id":"ed09ecf4-ecdb-467c-baea-3db8250dbdbf","email":"admin@test.com","userName":"admin","password":"admin123456","role":"manager"}]');
     this.usersService.usersData.subscribe(users => { this.users = users; });
   }
 
@@ -43,7 +37,6 @@ export class UsersHomeComponent implements OnInit {
 
   private onSuccessCallback(): void {
     this.usersService.getUsers().subscribe();
-    //this.router.navigate(['/users/home']);
   }
 
   private onErrorCallback(): void {
