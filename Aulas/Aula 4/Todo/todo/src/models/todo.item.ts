7bbb544c9fc488a02214ed8aed661098b7a296c3
George <george.hamann@gmail.com>
2022-06-29 17:45:00 -0300

    Readme and removed itens

--- Aulas/Aula 4/Todo/todo/src/models/todo.item.ts
+++
@@ -1,8 +0,0 @@
-export class TodoItem {
-  constructor(taskVal: string, completeVal: boolean = false) {
-    this.task = taskVal;
-    this.complete = completeVal;
-  }
-  task: string;
-  complete: boolean;
-}
